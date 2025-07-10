9fa2864a52fe5552bbfdc7f9c5015c566a4e62c2
Laavanya Dhasmana <113703044+laavanyadhasmana@users.noreply.github.com>
2025-07-10 21:23:00 +0530

    Add files via upload

---
+++ Cricket Player Detection/templates/script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+const spinner = document.getElementById('spinner');
+
+form.addEventListener('submit', () => {
+    spinner.classList.remove('hidden');
+});
+
+
+
+
